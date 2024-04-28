f73a4bcfc190576eb18e6570934f527cd75b38a1
Данила Печенин <dpechenin@icloud.com>
2024-04-28 15:53:36 +0300

    Initial Commit

---
+++ sort/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sort
+//
+//  Created by Данила Печенин on 28.04.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
